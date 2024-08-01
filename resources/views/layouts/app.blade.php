f3e390cba5ec37f5857255646c9d878a8134429d
Azzaz Khan <25636920+azzazkhan@users.noreply.github.com>
2024-08-02 01:59:48 +0500

    feat: Added toast notifications component

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,14 +14,14 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
+    <body class="font-sans antialiased" x-init="toast('hello world')">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
             <livewire:layout.navigation />
 
             <!-- Page Heading -->
             @if (isset($header))
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                <header class="bg-white shadow dark:bg-gray-800">
+                    <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
                 </header>
@@ -31,6 +31,10 @@
             <main>
                 {{ $slot }}
             </main>
+        </div>
+
+        <div class="relative w-auto h-auto">
+            <x-toast />
         </div>
     </body>
 </html>
